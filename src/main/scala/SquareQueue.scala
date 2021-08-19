70bb344fa1b7095dd92be4fd4d73b7d074776976
Gayatri <gayatri.singh@knoldus.com>
2021-08-19 15:45:28 +0530

    first commit

---
+++ src/main/scala/SquareQueue.scala
@@ -0,0 +1,18 @@
+class SquareQueue extends Queue
+{
+  override def enqueue(item: Double): String = {
+    if (queue_end == -1 && queue_start == -1) {
+      queue_start = queue_start + 1
+      queue_end = queue_end + 1
+      val ItemSquare = item * item
+      queue = queue ::: List(ItemSquare)
+      "Enqueued"
+    }
+    else {
+      queue_end = queue_end + 1
+      val ItemSquare = item * item
+      queue = queue ::: List(ItemSquare)
+      "Enqueued"
+    }
+  }
+}
